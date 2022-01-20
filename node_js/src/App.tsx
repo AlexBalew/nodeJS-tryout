7887b6b601f330f831f57a3aa5719a230c79c120
AlexBalew <balew48@gmail.com>
2022-01-20 20:22:43 +0300

    front

---
+++ node_js/src/App.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+    let [users, setUsers] = useState([])
+
+    const getUsers = () => {
+        axios.get('http://localhost:4848/users')
+            .then(res => setUsers(res.data))
+    }
+
+    const onCreateUser = () => {
+        axios.post('http://localhost:4848/users')
+            .then(res => {
+                getUsers()
+            })
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    return (
+        <div>
+            <button onClick={onCreateUser}>create user</button>
+            <div>{users.map(({name}) => <div>{name}</div>)}</div>
+        </div>
+    );
+}
+
+export default App;
